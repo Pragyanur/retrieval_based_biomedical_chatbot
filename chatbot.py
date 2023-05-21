395c3e81f4a46e2c7f042b3d7cd54d59ffbfa44e
Pragyanur <pragyanur.babu@gmail.com>
2023-05-21 11:44:31 +0530

    training model updated

--- chatbot.py
+++ chatbot.py
@@ -7,31 +7,19 @@ import numpy as np
 from nltk.stem import WordNetLemmatizer
 import keras
 from keras.models import load_model
-from training_model import words, ignore_words, classes, diseases
 
 nlp = spacy.load("en_core_sci_sm")
-model = load_model("intent_classification.h5", compile = False)
+model = load_model("intent_classification.h5", compile=False)
 
-text = """Myeloid derived suppressor cells (MDSC) are immature 
-myeloid cells with immunosuppressive activity. Lung cancer
-They accumulate in tumor-bearing mice and humans 
-with different types of cancer, including hepatocellular 
-carcinoma (HCC)."""
+words = pickle.load(open("pickles/words.pkl", "rb"))
+diseases = pickle.load(open("pickles/diseases.pkl", "rb"))
+classes = pickle.load(open("pickles/classes.pkl", "rb"))
 
-doc = nlp(text)
-
-entities = list(ent.text for ent in doc.ents)
-
-print(entities)
-
-def check_disease(msg):
+def predict_disease(msg):
     doc = nlp(msg)
     entities = list(ent.text for ent in doc.ents)
     for e in entities:
         if e in diseases:
             return e
-
-# if e not in list_of_diseases
-# response tag "none"
-
-print(check_disease("tuberculosis"))
+        else:
+            return "none"
--- training_model.py
+++ training_model.py
@@ -2,7 +2,6 @@ import random
 import json
 import pickle
 import nltk
-import spacy
 from nltk.stem import WordNetLemmatizer
 import numpy as np
 from keras.models import Sequential
@@ -12,13 +11,13 @@ from keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
 
-words = []                  # set of words in the dataset intents.json
-diseases = []               # diseases in responses.json
-documents = []              # list(words in pattern, corresponding tag)
-classes = []                # intent classes from intents.json
+words = []  # set of words in the dataset intents.json
+diseases = []  # diseases in responses.json
+documents = []  # list(words in pattern, corresponding tag)
+classes = []  # intent classes from intents.json
 ignore_words = ["?", "!", ".", ","]
 
-file = open("responses.json", "r", encoding = "UTF-8")
+file = open("responses.json", "r", encoding="UTF-8")
 data = json.load(file)
 
 for disease in data["responses"]:
@@ -26,12 +25,12 @@ for disease in data["responses"]:
     diseases.append(d)
 
 
-file = open("intents.json", "r", encoding = "UTF-8")
+file = open("intents.json", "r", encoding="UTF-8")
 intents = json.load(file)
 
 for intent in intents["intents"]:
     for pattern in intent["patterns"]:
-        w =  nltk.word_tokenize(pattern)
+        w = nltk.word_tokenize(pattern)
         words.extend(w)
         documents.append((w, intent["tag"]))
 
@@ -52,12 +51,12 @@ print("diseases: ", diseases)
 print("classes: ", classes)
 print("")
 
-pickle.dump(words, open("words.pkl", "wb"))
-pickle.dump(diseases, open("diseases.pkl", "wb"))
-pickle.dump(classes, open("classes.pkl", "wb"))
+pickle.dump(words, open("pickles/words.pkl", "wb"))
+pickle.dump(diseases, open("pickles/diseases.pkl", "wb"))
+pickle.dump(classes, open("pickles/classes.pkl", "wb"))
 
 training = []
-output_empty = [0] * len(classes) # symptoms, cure
+output_empty = [0] * len(classes)
 
 for doc in documents:
     bag = []
@@ -82,8 +81,18 @@ model.add(Dense(64, activation="relu"))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation="softmax"))
 
-sgd=SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
 
-HIST = model.fit(np.array(train_x), np.array(train_y), epochs=500, batch_size=5, verbose=1)
+HIST = model.fit(
+    np.array(train_x), np.array(train_y), epochs=500, batch_size=5, verbose=1
+)
 model.save("intent_classification.h5", HIST)
+
+
+
+# def exp_data(self, words, ignore_words, classes, diseases):
+#     self._words = words
+#     self._ignore_words = ignore_words
+#     self._classes = classes
+#     self._diseases = diseases
